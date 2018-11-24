286355a825f072da99302b188ecdcef11c66458f
Shubham <shubham.baghel@nagarro.com>
2018-11-24 22:00:31 +0530

    Added Home Page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
+import {Redirect} from 'react-router-dom';
 
 class Home extends Component {
+    constructor(props){
+        super(props);
+        this.onBeginQuiz = this.onBeginQuiz.bind(this);
+        this.state = {
+            isVisiter : false,
+        }
+    }
+
+    onBeginQuiz(){
+        this.setState({isVisiter : true});
+    }
+
     render() {
+        if(this.state.isVisiter){
+            return <Redirect to = "/visitorQuiz" />
+        }
+
         return (
-            <h1>Welcome to the Quiz-Up. Here you could find various topics to test your skill-set.
-                Explore!.
-            </h1>
+            <main role="main" className="inner cover">
+                <h1 className="cover-heading">Welcome to Quiz App.</h1>
+                <p className="lead">You can sharpen your skillset by giving quiz from various topics</p>
+                <p className="lead">
+                    <button className="btn btn-lg btn-primary" onClick={this.onBeginQuiz}>Let's Begin</button>
+                </p>
+            </main>
         )
     }
 }
