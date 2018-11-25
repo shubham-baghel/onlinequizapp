27f9d51c7f6ac576cdb934be6ffe8ddda99816a0
Narendra Kumawat <kmwtnarendra@gmail.com>
2018-11-25 22:26:27 +0530

    filter added

--- api/app/routes/controllers/QuestionController.js
+++ api/app/routes/controllers/QuestionController.js
@@ -17,14 +17,15 @@ router.post('/', function (req, res) {
 
     var answers=req.body.answers.map(function(val,index){return new Option({id:val.id,o:val.o})});
     var options=req.body.options.map(function(val,index){return new Option({id:val.id,o:val.o})});
-   
+    let tags=(req.body.tags||[]).map((val,index)=>val.toUpperCase());
+    let subjects=(req.body.subjects||[]).map((val,index)=>val.toUpperCase());
+
     Question.create({
             question : req.body.question,
             answers : answers,
             options : options,
-            subject:req.body.subject,
-            subjects:req.body.subjects,
-            tags:req.body.tags,
+            subjects:subjects,
+            tags:tags,
             language:req.body.language,
             level:req.body.level
         }, 
@@ -52,6 +53,18 @@ router.get('/t/(:arr)*', function (req, res) {
     var paramsArray = [req.params.arr.concat(params[0])].concat(params.slice(1,params.length));
 
     Question.find({tags:{ $elemMatch: { $in: paramsArray } }}, 
+        function (err, questions) {
+            if (err) return res.status(500).send("Error :"+JSON.stringify(err));
+            res.status(200).send(questions);
+        });
+});
+
+//GET QUESTIONS BY SUBJECTS OR TAGS
+router.post('/st', function (req, res) {
+    let subjects=(req.body.subjects||[]).map((val,index)=>val.toUpperCase());
+    let tags=(req.body.tags||[]).map((val,index)=>val.toUpperCase());
+
+    Question.find({ $or:[{tags:{ $elemMatch: { $in: tags }}},{subjects:{ $elemMatch: { $in: subjects }}}]}, 
         function (err, questions) {
             if (err) return res.status(500).send("Error :"+JSON.stringify(err));
             res.status(200).send(questions);
