d2d2441dfd172097a3b811d77ae9da136260f334
Shubham <shubham.baghel@nagarro.com>
2018-12-06 21:57:34 +0530

    UI -change

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -20,15 +20,10 @@ class Header extends Component {
             <header>
                 <div className="container-fluid bg-info">
                     <div className="row">
-                        <div className="col-sm-1 text-left">
-                            <div className="mt-3">
-                                <label className="label"><big><span className="glyphicon glyphicon-tasks"></span></big></label>
-                            </div>
-                        </div>
-                        <div className="col-sm-4">
+                        <div className="col-sm-12">
                             <ul role="tablist" className="nav nav-fill nav-tabs" id="navTabs">
-                                  <li className="nav-item">
-                                    <Link role="tab" aria-selected="true" className="nav-link active" to="/"><big><span className="glyphicon glyphicon-home"></span></big></Link>
+                                  <li className="nav-item text-left">
+                                    <Link role="tab" aria-selected="true" className="nav-link" to="/"><label className="label"><big><span className="glyphicon glyphicon-home"></span></big></label></Link>
                                 </li>
                                 <li className="nav-item">
                                     <Link onClick={this.onNavLinkClick} role="tab" aria-selected="false" className="nav-link" to="/quiz"><span className="label"><big>Quiz</big></span></Link>
@@ -39,12 +34,10 @@ class Header extends Component {
                                 <li role="tab" className="nav-item">
                                     <Link role="tab" aria-selected="false" className="nav-link" to="/signin"><span className="label"><big>Sign In</big></span></Link>
                                 </li>
+                                <li role="tab" className="nav-item text-right mt-3">
+                                    <label role="button" onClick={this.signOut.bind(this)} ><big><span className="glyphicon glyphicon-user"></span></big></label>  
+                                </li>
                             </ul>
-                        </div>
-                        <div className="col-sm-7 text-right">
-                            <div className="mt-3">
-                                <label role="button" onClick={this.signOut.bind(this)} ><big><span className="glyphicon glyphicon-user"></span></big></label>
-                            </div>
                         </div>
                     </div>
                 </div>
--- src/Components/QuizLaunch/QuizLaunchByVisiter.js
+++ src/Components/QuizLaunch/QuizLaunchByVisiter.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Redirect} from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import withAuth from '../Shared/withAuth';
 
 
@@ -9,81 +9,98 @@ class QuizLaunchByVisitor extends Component {
         this.handleSelection = this.handleSelection.bind(this);
         this.submitCriteria = this.submitCriteria.bind(this);
         this.state = {
-            quizCriteria : {
-                selectedSubjects : [],
-                selectedTags : []
+            quizCriteria: {
+                selectedSubjects: [],
+                selectedTags: []
             },
-            renderQuiz : false,
+            renderQuiz: false,
         }
     }
 
-    handleSelection(e) {
-        let target = e.target;
+    handleSelection(buttonType, value) {
         let criteria = Object.assign({}, this.state.quizCriteria);
-        if (target.getAttribute("buttonType") == "subject") {
-            if (criteria.selectedSubjects.includes(target.value)) {
-                criteria.selectedSubjects.splice(criteria.selectedSubjects.indexOf(target.value), 1);
+        if (buttonType == "subject") {
+            if (criteria.selectedSubjects.includes(value)) {
+                criteria.selectedSubjects.splice(criteria.selectedSubjects.indexOf(value), 1);
             } else {
-                criteria.selectedSubjects.push(target.value);
+                criteria.selectedSubjects.push(value);
             }
         } else {
-            if (criteria.selectedTags.includes(target.value)) {
-                criteria.selectedTags.splice(criteria.selectedTags.indexOf(target.value), 1);
-            }else {
-            criteria.selectedTags.push(target.value);
+            if (criteria.selectedTags.includes(value)) {
+                criteria.selectedTags.splice(criteria.selectedTags.indexOf(value), 1);
+            } else {
+                criteria.selectedTags.push(value);
             }
         }
         this.setState({ quizCriteria: criteria });
     }
 
-    submitCriteria(){
-        this.setState({renderQuiz : true});
+    submitCriteria() {
+        this.setState({ renderQuiz: true });
     }
 
     render() {
-        if(this.state.renderQuiz){
+        if (this.state.renderQuiz) {
             return <Redirect to=
+                {
                     {
-                    {pathname :"/quiz",
-                     state : {quizCriteria : this.state.quizCriteria}}}/>
+                        pathname: "/quiz",
+                        state: { quizCriteria: this.state.quizCriteria }
+                    }} />
         }
-       
-        let subjects = ['gk', 'science', 'history'];
-        let tags = ['electro', 'organic-chemistry', 'tag'];
+
+        let subjects = ['gk', 'science', 'history', 'gk', 'science', 'history', 'gk', 'gk', 'science', 'history', 'gk', 'science', 'history', 'gk', 'science', 'history', 'gk', 'science', 'history', 'gk', 'science', 'history'];
+        let tags = ['electro', 'organic-chemistry', 'tag', 'electro', 'organic-chemistry', 'tag', 'electro', 'organic-chemistry', 'tag', 'electro', 'organic-chemistry', 'tag', 'electro', 'organic-chemistry', 'tag', 'electro', 'organic-chemistry', 'tag'];
 
         return (
-            <div className="container">
-                <div className="jumbotron">
-                    <div><b>Select Subjects</b></div>
-                    <div className="row">
-                        <div className="btn-toolbar">
-                            {
-                                subjects.map((val, i) => {
-                                    return (<button type="button" value={val} buttonType="subject" className={this.state.quizCriteria.selectedSubjects.includes(val) == true ?  "btn btn-success" : "btn btn-primary"} onClick={this.handleSelection}>{val}</button>)
-                                })
-                            }
+            <div className="container-fluid mt-4">
+                <div className="row">
+                    <div className="col-lg-3"> </div>
+                    <div className="col-lg-6">
+                        <div className="jumbotron">
+                            <div className="text-left"><label className="label label-default">Select Subjects</label></div>
+                            <div className="row">
+                                <div className="btn-toolbar">
+                                    {
+                                        subjects.map((val, i) => {
+                                            return (<button key={i} type="button" value={val} className={this.state.quizCriteria.selectedSubjects.includes(val) == true ? "btn btn-success m-2" : "m-2 btn btn-primary"} onClick={() => this.handleSelection("subject", val)}>{val}</button>)
+                                        })
+                                    }
+                                </div>
+                            </div>
                         </div>
                     </div>
+                    <div className="col-lg-3"></div>
                 </div>
-
-                <div className="jumbotron">
-                    <div><b>Select Tags</b></div>
-                    <div className="row">
-                        <div className="btn-toolbar">
-                            {
-                                tags.map((val, i) => {
-                                    return (<button type="button" value={val} buttonType="tag" className={this.state.quizCriteria.selectedTags.includes(val) == true ?  "btn btn-success" : "btn btn-primary"} onClick={this.handleSelection}>{val}</button>)
-                                })
-                            }
+                <div className="row">
+                    <div className="col-lg-3"> </div>
+                    <div className="col-lg-6">
+                        <div className="jumbotron">
+                            <div className="text-left"><label className="label label-default">Select Tags</label></div>
+                            <div className="row">
+                                <div className="btn-toolbar">
+                                    {
+                                        tags.map((val, i) => {
+                                            return (<button key={i} type="button" value={val} className={this.state.quizCriteria.selectedTags.includes(val) == true ? "m-2 btn btn-success" : "m-2 btn btn-primary"} onClick={() => this.handleSelection("tag", val)}>{val}</button>)
+                                        })
+                                    }
+                                </div>
+                            </div>
                         </div>
                     </div>
+                    <div className="col-lg-3"></div>
                 </div>
-                <div>
-                    <button type="submit" className="btn btn-primary" disabled={this.state.quizCriteria.selectedSubjects.length <= 0 && this.state.quizCriteria.selectedTags.length <= 0  } onClick={this.submitCriteria}>Submit</button>
+                <div className="row">
+                    <div className="col-lg-3"> </div>
+                    <div className="col-lg-6">
+                        <div>
+                            <button type="submit" className="btn btn-primary" disabled={this.state.quizCriteria.selectedSubjects.length <= 0 && this.state.quizCriteria.selectedTags.length <= 0} onClick={this.submitCriteria}>Submit</button>
+                        </div>
+                    </div>
+                    <div className="col-lg-3"></div>
                 </div>
             </div>
         )
-
     }
 }
 
