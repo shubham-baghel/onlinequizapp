5f54f7d52b2f41f00177f0a4e2819e907cb28dec
Narendra K <kmwtnarendra@gmail.com>
2018-12-11 17:54:31 +0530

    header ui responsive.

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -18,19 +18,26 @@ class Header extends Component {
     render() {
         return (
             <header>
-                <div className="container-fluid App-header">
-                    <div className="row">
-                        <div className="col-sm-12">
-                            <ul role="tablist" className="nav nav-fill nav-tabs border-0" id="navTabs">
-                                <li className="nav-item text-left">
-                                    <Link role="tab" aria-selected="true" className="nav-link" to="/"><label className="label"><big><span className="glyphicon glyphicon-home"></span></big></label></Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link onClick={this.onNavLinkClick} role="tab" aria-selected="false" className="nav-link" to="/quiz"><span className="label"><big>Quiz</big></span></Link>
-                                </li>
-                                <li role="tab" className="nav-item text-right">
-                                    <Link role="tab" aria-selected="false" className="nav-link" to="/user_dashboard"><label className="label"><big><span className="glyphicon glyphicon-user"></span></big></label></Link>
-                                    {/* <label role="button" onClick={this.signOut.bind(this)} ><span className="glyphicon glyphicon-user"></span></label>   */}
+               <div className="App-header navbar navbar-inverse">
+                    <div className="container-fluid d-inline">
+                        <div className="navbar-header">
+                            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                            <Link className="navbar-brand" to="/"><label className="label"><big><span className="glyphicon glyphicon-home"></span></big></label></Link>
+                        </div>
+                        <div className="navbar-collapse collapse">
+                            <ul className="nav navbar-nav">
+                                <li><Link onClick={this.onNavLinkClick}  to="/quiz"><span className="label"><big>Quiz</big></span></Link></li>
+                            </ul>
+                            <ul className="nav navbar-nav navbar-right d-inline">
+                                <li>
+                                    <Link  to="/user_dashboard"><label className="label"><big><span className="glyphicon glyphicon-user"></span></big></label></Link>
+                                </li> 
+                                <li>
+                                    <Link title="logout" to="/"><label className="label"><big><span className="glyphicon glyphicon-log-out"></span></big></label></Link>
                                 </li>
                             </ul>
                         </div>
