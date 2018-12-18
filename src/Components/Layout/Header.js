5cda9bc2f345d7144b30ddb1d441261c21b5afa8
Narendra K <kmwtnarendra@gmail.com>
2018-12-18 22:06:38 +0530

    Header corrected.

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -5,44 +5,41 @@ import AuthService from '../../Services/AuthService';
 class Header extends Component {
     constructor(props) {
         super(props);
-        this.onNavLinkClick=this.onNavLinkClick.bind(this);
         this.authService = new AuthService();
     }
 
     signOut() {
         this.authService.logout();
     }
-    onNavLinkClick(e){
-
-    }
+    
     render() {
         return (
             <header>
-               <div className="App-header navbar navbar-inverse">
-                    <div className="container-fluid d-inline">
-                        <div className="navbar-header">
-                            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-                            </button>
-                            <Link className="navbar-brand" to="/"><label className="label"><big><span className="glyphicon glyphicon-home"></span></big></label></Link>
-                        </div>
-                        <div className="navbar-collapse collapse">
-                            <ul className="nav navbar-nav">
-                                <li><Link onClick={this.onNavLinkClick}  to="/quiz"><span className="label"><big>Quiz</big></span></Link></li>
-                            </ul>
-                            <ul className="nav navbar-nav navbar-right d-inline">
-                                <li>
-                                    <Link  to="/user_dashboard"><label className="label"><big><span className="glyphicon glyphicon-user"></span></big></label></Link>
-                                </li> 
-                                <li>
-                                    <Link title="logout" to="/"><label className="label"><big><span className="glyphicon glyphicon-log-out"></span></big></label></Link>
-                                </li>
-                            </ul>
-                        </div>
+                <nav className="navbar navbar-expand-lg navbar-light App-header">
+                    <Link className="navbar-brand App-logo" to="/">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Link>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/quiz">Quiz</Link>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <span className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    User
+                               </span>
+                                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <Link className="dropdown-item App-link" to="/user_dashboard">Dashboard</Link>
+                                    <Link className="dropdown-item App-link" to="/">Link</Link>
+                                    <div className="dropdown-divider"></div>
+                                    <Link className="dropdown-item App-link" title="logout" to="/">Logout</Link>
+                                </div>
+                            </li>
+                        </ul>
                     </div>
-                </div>
+                </nav>
             </header>
         )
     }
